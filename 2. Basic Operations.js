c2e1650ec33910c40be91e8b6feef5eff8f87f81
Abhishek Sarwan <abhisheksarwan@icloud.com>
2020-03-30 21:07:34 +0530

    Update 2. Basic Operations.js

--- 2. Basic Operations.js
+++ 2. Basic Operations.js
@@ -1,12 +1,14 @@
+//on numbers
 var num1 = 10;
 var num2 = 20;
 
 num1 + num2; //addition
-num1 / num2; //addition
-num1 * num2; //addition
+num1 / num2; //subtraction
+num1 * num2; //multiplication
 num1 / num2; //division
-num1 % num2; //addition
+num1 % num2; //modulo
 
+//on strings
 var str1 = "Hello";
 var str2 = "World";
 
