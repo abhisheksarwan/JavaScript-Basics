fb283ea388e46fb44efe3e16388c5c90de3f0711
Abhishek Sarwan <abhisheksarwan@icloud.com>
2020-03-30 21:08:43 +0530

    Update 3. Simple Methods to Display.js

--- 3. Simple Methods to Display.js
+++ 3. Simple Methods to Display.js
@@ -4,11 +4,12 @@ alert(10+10);
 
 //prompt
 prompt("What's your age?"); //simple pop-up but takes input
-    var var1 = prompt("First Name?");
-    var var2 = prompt("Last Name?");
-    alert("Your full name is " + var1 + " " + var2);
 
- //by default prompt takes input as a string to convert it into a number user Number()   
+//use of prompt
+var var1 = prompt("First Name?");
+var var2 = prompt("Last Name?");
+alert("Your full name is " + var1 + " " + var2);
+//by default prompt takes input as a string to convert it into a number user Number()   
 
 //console.log
 console.log("Hey"); //visible only in console
