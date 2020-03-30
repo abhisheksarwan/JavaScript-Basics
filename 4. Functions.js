e95f9897892da1e86a0b2622747d5a01a3a4d244
Abhishek Sarwan <abhisheksarwan@icloud.com>
2020-03-30 21:09:40 +0530

    Update 4. Functions.js

--- 4. Functions.js
+++ 4. Functions.js
@@ -1,18 +1,17 @@
-function sing() {       //function decleration and definition
+//simple function
+function sing() {            //function decleration and definition
     console.log("Hey");
 } 
 sing();         //call function
 
 //function expression
-
-var func1 = function() {        //function decleration and definition
+var func1 = function() {    //function decleration and definition
     console.log("LaLaLa");
 }
 func1();         //call function
 
 //new syntax or arrow function
-
-var func1 = () => {        //function decleration and definition
+var func1 = () => {          //function decleration and definition
     console.log("LaLaLa");
 }
 func1(); 
